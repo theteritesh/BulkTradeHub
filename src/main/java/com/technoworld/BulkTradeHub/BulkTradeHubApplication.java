916b35569373c7f079de02b4d3f74da22cbebe8e
theteritesh <theteritesh2@gmail.com>
2025-01-30 17:42:01 +0530

    First Commit

---
+++ src/main/java/com/technoworld/BulkTradeHub/BulkTradeHubApplication.java
@@ -0,0 +1,13 @@
+package com.technoworld.BulkTradeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkTradeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkTradeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technoworld/BulkTradeHub/BulkTradeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technoworld.BulkTradeHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkTradeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
