13ad4e23a33c94db27a05e97c5103f4730ffbdf8
theteritesh <theteritesh2@gmail.com>
2025-03-08 12:49:12 +0530

    showProduct table added

--- src/main/java/com/technoworld/BulkTradeHub/retailshop/controller/DashboardController.java
+++ src/main/java/com/technoworld/BulkTradeHub/retailshop/controller/DashboardController.java
@@ -1,5 +1,7 @@
 package com.technoworld.BulkTradeHub.retailshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.technoworld.BulkTradeHub.retailshop.entity.Product;
+import com.technoworld.BulkTradeHub.retailshop.service.ProductService;
 
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 	
+	private final ProductService productService;
+	
+	
+	public DashboardController(ProductService productService) {
+		this.productService = productService;
+	}
+
 	@GetMapping("")
 	public String displayDashboard() {
 		return "/retailshop/dashboard";
@@ -24,4 +34,11 @@ public class DashboardController {
 	    model.addAttribute("successMessage", successMessage);
 		return "/retailshop/addProduct";
 	}
+	
+	@GetMapping("/showProducts")
+    public String displayProducts(Model model) {
+        List<Product> productList = productService.getAllProducts();
+        model.addAttribute("products", productList);
+        return "/retailshop/showProduct";
+    }
 }
--- src/main/java/com/technoworld/BulkTradeHub/retailshop/service/ProductService.java
+++ src/main/java/com/technoworld/BulkTradeHub/retailshop/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.technoworld.BulkTradeHub.retailshop.service;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.technoworld.BulkTradeHub.retailshop.entity.Product;
@@ -19,4 +21,17 @@ public class ProductService {
     public void saveProduct(Product product) {
         productRepository.save(product);
     }   
+    
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+    
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
+    
 }
