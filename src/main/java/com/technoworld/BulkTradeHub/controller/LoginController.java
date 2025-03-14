da948fb9fcbe11158b5f0de05f285a32eb1b1c56
theteritesh <theteritesh2@gmail.com>
2025-03-14 14:56:44 +0530

    csrf added in project

--- src/main/java/com/technoworld/BulkTradeHub/controller/LoginController.java
+++ src/main/java/com/technoworld/BulkTradeHub/controller/LoginController.java
@@ -1,10 +1,17 @@
 package com.technoworld.BulkTradeHub.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -23,5 +30,14 @@ public class LoginController {
 		
 		model.addAttribute("errorMessage",errorMessage);
 		return"login";
+	}
+	
+	@GetMapping("/logout")
+	public String logoutPage(HttpServletRequest request,HttpServletResponse response) {
+		Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication);
+		}
+		return "redirect:/login?logout=true";
 	}
 }
