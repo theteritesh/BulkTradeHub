e578c5ef20d4ef42c67c5be50b550633345c7eb7
theteritesh <theteritesh2@gmail.com>
2025-04-01 00:27:58 +0530

    New index page added and new arrival product shown

--- src/main/java/com/technoworld/BulkTradeHub/controller/HomeController.java
+++ src/main/java/com/technoworld/BulkTradeHub/controller/HomeController.java
@@ -1,13 +1,60 @@
 package com.technoworld.BulkTradeHub.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.technoworld.BulkTradeHub.entity.Product;
+import com.technoworld.BulkTradeHub.entity.ProductPost;
+import com.technoworld.BulkTradeHub.repository.ProductPostRepository;
+import com.technoworld.BulkTradeHub.repository.ProductRepository;
+
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
-	@GetMapping(value = {"/","home"})
-	public String displayHome() {
-		return "index";
+	@Autowired
+	private ProductPostRepository productPostRepository; 
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping("")
+    public String displayHome(@RequestParam(defaultValue = "0") int page, Model model) {
+        int pageSize = 16;  // Show 16 products per page
+        Page<ProductPost> newArrivalProductsPage = productPostRepository.findAllByOrderByPostedAtDesc(PageRequest.of(page, pageSize));
+
+        model.addAttribute("newArrivalProductsPage", newArrivalProductsPage);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", newArrivalProductsPage.getTotalPages());
+
+        return "index";
+    }
+	
+	@GetMapping("/product/image/{id}")
+	@ResponseBody
+	public ResponseEntity<byte[]> getDefaultProductImage(@PathVariable Long id) {
+	    Optional<Product> productOptional = productRepository.findById(id);
+
+	    if (productOptional.isPresent()) {
+	        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(productOptional.get().getMainImage());
+	    }
+
+	    return ResponseEntity.notFound().build();
 	}
+
+	
+	
 }
--- src/main/java/com/technoworld/BulkTradeHub/repository/ProductPostRepository.java
+++ src/main/java/com/technoworld/BulkTradeHub/repository/ProductPostRepository.java
@@ -1,5 +1,8 @@
 package com.technoworld.BulkTradeHub.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,6 @@ import com.technoworld.BulkTradeHub.entity.ProductPost;
 
 @Repository
 public interface ProductPostRepository extends JpaRepository<ProductPost, Integer> {
+
+	Page<ProductPost> findAllByOrderByPostedAtDesc(Pageable pageable);
 }
